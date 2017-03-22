ccb10a9a615ab69307a9f5e176f830b32744e4ec
Luke Garrison <lgarriso@nd.edu>
2017-03-21 23:15:40 -0400

    Formats the RSVP section of the admin page

--- src/admin/js/main.js
+++ src/admin/js/main.js
@@ -16,10 +16,13 @@ $(window).on('load', function() {
 		} else {
 			data.responses.forEach(function(response) {
 
-				var responseElement = document.createElement("p");
-				responseElement.innerHTML = response.Name;
-				console.log(response);
+				// var responseElement = document.createElement("p");
+				// responseElement.innerHTML = response.Name;
+				// console.log(response);
+				console.log(createResponseElement(response));
+				var responseElement = createResponseElement(response);
 				$('#responses-container').append(responseElement);
+
 			});
 		}
 	}).fail(function (jqXHR, textStatus, errorThrown) {
@@ -35,13 +38,50 @@ $(window).on('load', function() {
 
 		} else {
 			data.responses.forEach(function(response) {
-				var responseElement = document.createElement("p");
-				responseElement.innerHTML = response.Song;
-				console.log(response);
-				$('#song-responses-container').append(responseElement);
+				var songElement = createSongElement(response);
+				$('#song-responses-container').append(songElement);
 			});
 		}
 	}).fail(function (jqXHR, textStatus, errorThrown) {
 		console.log(jqXHR.responseJSON.message);
 	});
 });
+
+// returns the html string for an rsvp response
+function createResponseElement(response) {
+	var attendingIconHtml;
+	if (response.IsAttending) {
+		attendingIconHtml = '<i class="glyphicon glyphicon-ok circle-icon circle-icon-green"></i>';
+	} else {
+		attendingIconHtml = '<i class="glyphicon glyphicon-remove circle-icon circle-icon-red"></i>';
+	}
+
+	var html = [
+		'<div class="response-container">',
+			'<div class="attending-icon-container">',
+				attendingIconHtml,
+			'</div>',
+			'<span class="num-guests">',
+				response.NumGuests,
+			'</span>',
+			'<span class="guest-name">',
+				response.Name,
+			'</span>',
+		'</div>'
+	];
+
+	return html.join(" ");
+}
+
+// returns the html string for a song
+function createSongElement(song) {
+	var html = [
+		'<div class="response-container">',
+			'<span class="song-title">',
+				song.Song,
+			'</span>',
+		'</div>'
+	];
+
+	return html.join(' ');
+}
